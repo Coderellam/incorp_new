9ae56ec5ed40fc32f3ba04bd582d98ee4653b0d0
coderellam <gulmira929@gmail.com>
2023-10-23 23:13:33 +0500

    last commit

--- app/models.py
+++ app/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-service_type = [('ux-ui', 'ux-ui'),
-                ('web-development', 'web-development'),
-                ('internet-platform', 'internet-platform'), ('mobile-development', 'mobile-development')
-                ]
-
-
 class Contact(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
@@ -16,12 +10,11 @@ class Contact(models.Model):
     message = models.TextField()
     is_solved = models.BooleanField(default=False)
 
-    service = models.CharField(max_length=100, unique=True, choices=service_type)
-
     web_development = models.BooleanField(default=False)
     ux_ui = models.BooleanField(default=False)
     internet_platform = models.BooleanField(default=False)
     mobile_development = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -30,6 +23,9 @@ class Contact(models.Model):
 class Portfolio(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to='images/')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f'name: {self.name}'
+
