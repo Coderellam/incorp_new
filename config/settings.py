6fb199c395825d3f03fad352d1b98c2f70b02c07
coderellam <gulmira929@gmail.com>
2023-10-27 22:37:35 +0500

    debug changed to True

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)_jcz^@ak_%x48(%xia%kn8r5$$&%&&(ztinqfa&e$@d#)g1$!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
